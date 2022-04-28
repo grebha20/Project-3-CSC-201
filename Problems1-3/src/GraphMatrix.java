e011d16885a6d587bb18a3b0a022cecc957f1cc7
grebha20 <grebha20@wfu.edu>
2022-04-27 20:47:05 -0400

    Finished 1 and 2, gave up on 3

--- Problems1-3/src/GraphMatrix.java
+++ Problems1-3/src/GraphMatrix.java
@@ -1,10 +1,10 @@
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class GraphMatrix implements Graph
 {
+    //initial declaration of array graph, edgecount, and visited array
     private int[][] array;
     private int edgecount;
     private boolean[] Visited;
@@ -25,14 +25,15 @@ public class GraphMatrix implements Graph
         return array.length;
     }
 
-    //returns variable edgecount in constant time
+    //returns edgecount in constant time
     @Override
     public int edgeCount()
     {
         return edgecount;
     }
 
-    //
+    //adds an edge between point v and w, with a given weight
+    //edgecount is updated
     @Override
     public void addEdge(int v, int w, int wgt)
     {
@@ -55,10 +56,12 @@ public class GraphMatrix implements Graph
     }
 
     //makes the weight equal to zero, removing the edge
+    //edgecount is updated
     @Override
     public void removeEdge(int v, int w)
     {
         array[v][w]=0;
+        edgecount--;
     }
 
     //returns true if there is an edge between v and w
@@ -77,6 +80,7 @@ public class GraphMatrix implements Graph
     @Override
     public int[] neighbors(int v)
     {
+        //adds the indices of the nodes v shares an edge with
         ArrayList<Integer> neighbor= new ArrayList<>();
         for(int i=0; i<nodeCount();i++)
         {
@@ -85,7 +89,9 @@ public class GraphMatrix implements Graph
                 neighbor.add(i);
             }
         }
-        int[] neighbor2=new int[neighbor.size()];
+
+        //converts values of neighbors from an arraylist to an array
+        int[] neighbor2= new int[neighbor.size()];
         for(int j=0; j<neighbor2.length;j++)
         {
             neighbor2[j]=neighbor.get(j);
@@ -104,21 +110,28 @@ public class GraphMatrix implements Graph
         }
     }
 
-    //
+    //Returns a list of integers, starting with Node v, and prints out all of v's nodes,
+    // and then the nodes of v's nodes, and so on.
     @Override
     public ArrayList<Integer> BFS(int v)
     {
         ArrayList<Integer> BFS=new ArrayList<>();
         BFS.add(v);
         Visited[v]=true;
+
+        //Creates queue of Nodes to visit and find the neighbors of
         Queue<Integer> toVisit=new LinkedList<>();
         toVisit.add(v);
 
+        //BFS is added to, and Visited is updated until the toVisit queue is empty
         while(!toVisit.isEmpty())
         {
             int[] list= neighbors(toVisit.remove());
             for(int i=0; i<list.length;i++)
             {
+                //If the index has not been visited, it is marked as having been visited, and
+                //it is added to the BFS list before its nodes, which are added to toVisit to be
+                // checked for neighbors
                 if(Visited[list[i]]==false)
                 {
                     Visited[list[i]]=true;
@@ -136,6 +149,8 @@ public class GraphMatrix implements Graph
     @Override
     public boolean hasPath(int v, int w)
     {
+        //rests visited to make sure no errors are made during BFS
+        //BFS(v) is run to check if it contains w
         resetVisited();
         ArrayList<Integer> bfs=BFS(v);
         if(bfs.contains(w))
@@ -145,10 +160,48 @@ public class GraphMatrix implements Graph
         return false;
     }
 
+
     @Override
     public ArrayList<Integer> topologicalSort()
     {
-        ArrayList<Integer> bfs= BFS(0);
+        resetVisited();
+
+        ArrayList<Integer> topo= new ArrayList<>();
+
+        //finds a Node that has no pointers towards it
+        boolean intake=false;
+        int i=0;
+        while(intake==false && i<array.length)
+        {
+            for(int j=0; j<array.length;j++)
+            {
+                if(array[i][j]>0)
+                {
+                    intake=true;
+                }
+            }
+            i++;
+        }
+
+        topo.add(i);
+        int[] list= neighbors(i);
+        Visited[i]=true;
+
+        boolean allVisited=false;
+        while(allVisited==false)
+        {
+
+
+            allVisited=true;
+            for(int j=0; j< Visited.length;j++)
+            {
+                if(Visited[j]==false)
+                {
+                    allVisited=false;
+                }
+            }
+        }
+
         return null;
     }
 }
